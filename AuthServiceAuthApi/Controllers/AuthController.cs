4c12e48589ad078fdf43ab77fca10262f862b891
Kirill <kirill.shlemen@gmail.com>
2021-02-10 22:47:23 +0300

    Add project files.

---
+++ AuthServiceAuthApi/Controllers/AuthController.cs
@@ -0,0 +1,97 @@
+﻿using AuthService.Auth.Common;
+using AuthServiceAuthApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthServiceAuthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IOptions<AuthOptions> authOptions;
+        public AuthController(IOptions<AuthOptions> authOptions)
+        {
+            this.authOptions = authOptions;
+        }
+        private List<Account> Accounts => new List<Account>
+        {
+            new Account()
+            { 
+                Id = Guid.Parse("beddd6fb-3d2b-4e6d-8c2d-beffcae1888e"),
+                Email = "user@email.com",
+                Password = "user",
+                Roles = new Role[]{ Role.User}
+            },
+            new Account()
+            {
+                Id = Guid.Parse("a599a0ac-19fa-4922-9f7d-f6799d0b9cce"),
+                Email = "user2@email.com",
+                Password = "user2",
+                Roles = new Role[]{ Role.User}
+            },
+            new Account()
+            {
+                Id = Guid.Parse("2e52fc0d-dd73-4911-a266-d9f636abf601"),
+                Email = "admin@email.com",
+                Password = "admin",
+                Roles = new Role[]{ Role.Admin}
+            }
+        };
+
+        [Route("login")]
+        [HttpPost]
+        public IActionResult Login([FromBody]Login request)
+        {
+            var user = AuthenticateUser(request.Email, request.Password);
+
+            if (user != null)
+            {
+                var token = GenerateJWT(user);
+
+                return Ok(new { access_token = token });
+            }
+
+            return Unauthorized();
+        }
+
+        private Account AuthenticateUser(string email, string password)
+        {
+            return Accounts.SingleOrDefault(u => u.Email == email && u.Password == password);
+        }
+
+        private string GenerateJWT(Account user)
+        {
+            var authParams = authOptions.Value;
+
+            var securityKey = authParams.GetSymmetricSecurityKey();
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>() {
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString())
+            };
+
+            foreach (var role in user.Roles)
+            {
+                claims.Add(new Claim("role", role.ToString()));
+            }
+
+            var token = new JwtSecurityToken(authParams.Issuer,
+                authParams.Audience,
+                claims,
+                expires: DateTime.Now.AddSeconds(authParams.TokenLifetime),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+} 
